4a045e4dde8bbc6accbc649312717f4ded5ab469
Sung Won Cho <mikeswcho@gmail.com>
2015-06-21 12:59:47 +1000

    Add footer

--- client/templates/shared/helpers.js
+++ client/templates/shared/helpers.js
@@ -4,3 +4,7 @@ Template.registerHelper('formatDate', function (date) {
   var day = date.substring(6,8);
   return moment(year + '-' + month + '-' + day).format('MMMM D, YYYY');
 });
+
+Template.registerHelper('currentYear', function () {
+  return moment().format('YYYY');
+});
