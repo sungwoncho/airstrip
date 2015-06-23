67858393de0a60b385472d87235f382a3361738c
Sung Won Cho <mikeswcho@gmail.com>
2015-06-23 15:41:40 +1000

    Get count and source only for items not hidden

--- client/templates/flights/flight.js
+++ client/templates/flights/flight.js
@@ -1,15 +1,23 @@
+getShownItems = function (flight) {
+  var shownItems = _.filter(flight.items, function (item) {
+    return item.hidden === false;
+  });
+
+  return shownItems;
+};
+
 Template.flight.helpers({
   recentFlights: function () {
     return Flights.find({}, {sort: {date: -1}});
   },
 
   itemCount: function () {
-    return this.items.length;
+    return getShownItems(this).length;
   },
 
   sources: function () {
-    var sources = _.pluck(this.items, 'source');
-    var uniqSources =_.uniq(sources);
+    var sources = _.pluck(getShownItems(this), 'source');
+    var uniqSources = _.uniq(sources);
     return uniqSources;
   }
 });
--- packages/rss/tests/rss-tests.js
+++ packages/rss/tests/rss-tests.js
@@ -1,7 +1,3 @@
 Tinytest.add('1+1 should equal 2', function (test) {
-  test.equal(1+1, 2)
+  fetchItems();
 });
-
-Tinytest.add('test2', function (test) {
-  test.equal(1+1, 2)
-})
