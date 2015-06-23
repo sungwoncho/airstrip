d3e0085dc60c6c516876b2f7dea0f97a76e35446
Sung Won Cho <mikeswcho@gmail.com>
2015-06-23 10:22:43 +1000

    Add captains log

--- client/templates/flights/flight.js
+++ client/templates/flights/flight.js
@@ -1,5 +1,15 @@
 Template.flight.helpers({
   recentFlights: function () {
     return Flights.find({}, {sort: {date: -1}});
+  },
+
+  itemCount: function () {
+    return this.items.length;
+  },
+
+  sources: function () {
+    var sources = _.pluck(this.items, 'source');
+    var uniqSources =_.uniq(sources);
+    return uniqSources;
   }
 });
