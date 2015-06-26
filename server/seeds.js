84fa385adabdb1c0b904fa73a3b6542c571b32d6
Sung Won Cho <mikeswcho@gmail.com>
2015-06-26 12:10:30 +1000

    Include uniq guid in seed data

--- server/seeds.js
+++ server/seeds.js
@@ -11,6 +11,7 @@ Meteor.startup(function () {
               title: Fake.sentence(5),
               source: 'www.reddit.com',
               url: 'http://www.reddit.com/r/digitalnomad/comments/39ytta/asking_employer_to_allow_work_remotely_for_travel/',
+              guid: 'http://www.reddit.com/r/digitalnomad/comments/39ytta/asking_employer_to_allow_work_remotely_for_travel/' + i,
               author: 'yayman91',
               hidden: false
             },
@@ -18,6 +19,7 @@ Meteor.startup(function () {
               title: Fake.sentence(5),
               source: 'some.blog.com',
               url: 'https://some.blog.com/post/123',
+              guid: 'https://some.blog.com/post/123' + i,
               author: 'pdfrules',
               hidden: false
             },
@@ -25,6 +27,7 @@ Meteor.startup(function () {
               title: Fake.sentence(5),
               source: 'www.nomadforum.io',
               url: 'https://nomadforum.io/t/digital-nomad-101-how-to-become-a-digital-nomad/465',
+              guid: 'https://nomadforum.io/t/digital-nomad-101-how-to-become-a-digital-nomad/465' + i,
               author: 'jane005',
               hidden: false
             },
@@ -32,6 +35,7 @@ Meteor.startup(function () {
               title: Fake.sentence(5),
               source: 'www.nomadlist.com',
               url: 'https://nomadlist.com/stories/iwantmyname-works-remotely?utm_content=buffer6e883&utm_medium=social&utm_source=twitter.com&utm_campaign=buffer',
+              guid: 'https://nomadlist.com/stories/iwantmyname-works-remotely?utm_content=buffer6e883&utm_medium=social&utm_source=twitter.com&utm_campaign=buffer' + i,
               author: 'Jake Ellenburger',
               hidden: false
             }
