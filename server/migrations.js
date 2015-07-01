aead0776565d10dab4ff10f0ec87acd85f1732ae
Sung Won Cho <mikeswcho@gmail.com>
2015-07-01 12:25:13 +1000

    Add migrations for item collection restructure

---
+++ server/migrations.js
@@ -0,0 +1,28 @@
+Migrations.add({
+  version: 1,
+  name: "Extract items to a separate collection from the nested field",
+  up: function () {
+    Flights.find({items: {$exists: true}}).forEach(function (flight) {
+      flight.items.forEach(function (item) {
+        var itemId = Items.insert(_.extend(item, {createdAt: new Date(), flightId: flight._id}));
+        Flights.update({_id: flight._id}, {$addToSet: {itemIds: itemId}});
+      });
+    });
+  },
+  down: function () {
+
+  }
+});
+
+Migrations.add({
+  version: 2,
+  name: "Drop items field",
+  up: function () {
+    Flights.update({items: {$exists: true}}, {$unset: {items: ''}});
+  },
+  down: function () {
+
+  }
+});
+
+Migrations.migrateTo('latest');
