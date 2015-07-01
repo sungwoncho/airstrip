b9260b3b99a94b838db2e839bed4d31106282cab
Sung Won Cho <mikeswcho@gmail.com>
2015-07-01 15:16:05 +1000

    Correctly scope the item count and source to the current flight

--- modules/flights/client/flight.js
+++ modules/flights/client/flight.js
@@ -1,21 +1,25 @@
+var getItemsForFlight = function (flight) {
+  return Items.find({'hidden': false, flightId: flight._id});
+};
+
 Template.flight.helpers({
   recentFlights: function () {
     return Flights.find({}, {sort: {date: -1}, limit: 4});
   },
 
   itemCount: function () {
-    return Items.find({'hidden': false}).count();
+    return getItemsForFlight(this).count();
   },
 
   sources: function () {
-    var items = Items.find({'hidden': false}).fetch();
+    var items = getItemsForFlight(this).fetch();
     var sources = _.pluck(items, 'source');
     var uniqSources = _.uniq(sources);
     return uniqSources;
   },
 
   items: function () {
-    return Items.find({'hidden': false, flightId: this._id});
+    return getItemsForFlight(this);
   }
 });
 
