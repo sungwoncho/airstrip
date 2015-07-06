4f0dcbed6426abf17ef3d465f496e9eb23a02e87
Sung Won Cho <mikeswcho@gmail.com>
2015-07-06 11:54:10 +1000

    Implement basic interface for campaignBuilder

---
+++ modules/newsletter/server/cron.js
@@ -0,0 +1,12 @@
+SyncedCron.add({
+  name: 'Schedule a daily digest',
+  schedule: function (parser) {
+    return parser.text('at 10:00 am');
+  },
+  job: function () {
+    var latestFlight = Flights.findOne({}, {sort: {date: -1}, limit: 1});
+
+    var campaign = campaignBuilder.run(latestFlight);
+    newsletterScheduler.run(campaign);
+  }
+});
