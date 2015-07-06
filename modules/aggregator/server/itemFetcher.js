8c499f694c3a0c012039d20b6207d97e61e8ce0d
Sung Won Cho <mikeswcho@gmail.com>
2015-07-06 18:16:24 +1000

    Add console output for debugging

--- modules/aggregator/server/itemFetcher.js
+++ modules/aggregator/server/itemFetcher.js
@@ -7,8 +7,10 @@ var itemLimitPerFlight = 10;
 ItemFetcher = {
   fetch: function () {
     // Create a flight if no flight exists for today
+    console.log('creating a flight for ' + today + '...');
     if (Flights.find({date: today}).count() === 0) {
       Meteor.call('createFlight', {date: today, itemIds: []});
+      console.log('flight created successfully.');
     }
 
     Feeds.find({}, {sort: {position: 1}}).forEach(function (feed) {
